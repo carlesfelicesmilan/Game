80c0017ce242447d9632997aeb5b3c1fb4b94716
Carles <carles.felices@gmail.com>
2020-04-14 17:38:14 +0200

    Added damage and potions

--- app/src/main/java/com/example/game/sprites/Character/Hero.java
+++ app/src/main/java/com/example/game/sprites/Character/Hero.java
@@ -5,10 +5,19 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
 import com.example.game.R;
+import com.example.game.sprites.Sprite;
 
-public class Hero extends Human {
+public class Hero implements Sprite {
 
     private int level, totalCoins;
+    private Numbers HeroHpValue, numberOfPotions;
+    private int width, height, posX, posY, posDamageY, potions;
+    private int hp, def, receivedDamage, atq;
+    private Bitmap terrexBase;
+    private Bitmap terrexHit;
+    private Bitmap potionRed;
+    private boolean hit;
+
     public Hero(Resources resources, int screenHeight, int screenWidth) {
         width = (int) resources.getDimension(R.dimen.button_width);
         height = (int) resources.getDimension(R.dimen.button_height);
@@ -17,17 +26,75 @@ public class Hero extends Human {
         hp = 100;
         atq = 7;
 
-        human = BitmapFactory.decodeResource(resources, R.drawable.terrex);
-        human = Bitmap.createScaledBitmap(human, width, height, false);
+        terrexBase = BitmapFactory.decodeResource(resources, R.drawable.terrex);
+        terrexBase = Bitmap.createScaledBitmap(terrexBase, width, height, false);
 
-        human1 = BitmapFactory.decodeResource(resources, R.drawable.terrexhit1);
-        human1 = Bitmap.createScaledBitmap(human1, width, height, false);
+        terrexHit = BitmapFactory.decodeResource(resources, R.drawable.terrexhit3);
+        terrexHit = Bitmap.createScaledBitmap(terrexHit, width, height, false);
 
-        human2 = BitmapFactory.decodeResource(resources, R.drawable.terrexhit2);
-        human2 = Bitmap.createScaledBitmap(human2, width, height, false);
+        potionRed = BitmapFactory.decodeResource(resources, R.drawable.redpotion);
+        potionRed = Bitmap.createScaledBitmap(potionRed, width, height, false);
 
-        human3 = BitmapFactory.decodeResource(resources, R.drawable.terrexhit3);
-        human3 = Bitmap.createScaledBitmap(human3, width, height, false);
+        HeroHpValue = new Numbers(resources, screenHeight, screenWidth);
+        HeroHpValue.updateHp(hp);
+
+        HeroHpValue.setHpPos(posX + width / 2, posY - height / 2);
+        HeroHpValue.setDamagePos(0, posY - height);
+
+        potions = 3;
+        numberOfPotions = new Numbers(resources, screenHeight, screenWidth);
+        numberOfPotions.updateHp(potions);
+        numberOfPotions.setHpPos(posX + width, posY + height);
     }
 
+    public void draw(Canvas canvas) {
+        HeroHpValue.drawHp(canvas);
+        if(hit) {
+            HeroHpValue.drawDamage(canvas);
+            canvas.drawBitmap(terrexHit, posX, posY, null);
+        }
+        else {
+            canvas.drawBitmap(terrexBase, posX, posY, null);
+        }
+
+        if(potions > 0) {
+            numberOfPotions.drawHp(canvas);
+            canvas.drawBitmap(potionRed, posX, posY + height, null);
+        }
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public boolean isDead() {
+        if (hp<0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int atq() {
+        return atq;
+    }
+
+    public void usePotion() {
+        potions--;
+        hp+=50;
+        HeroHpValue.updateHp(hp);
+        numberOfPotions.updateHp(potions);
+    }
+
+    public boolean isPotionUsed(int evX, int evY) {
+        if (evX >= posX && evX < (posX + width) && evY >= posY + height && evY < (posY + height*2)) {
+            //tada, if this is true, you've started your click inside your bitmap
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
